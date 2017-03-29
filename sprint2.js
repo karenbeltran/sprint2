a4ce4b798c797c0db893ee208b31104808549dd2
karenbeltran <beltranramirezkaren@gmail.com>
2017-03-29 20:01:16 +0000

    parte2

--- sprint2.js
+++ sprint2.js
@@ -5,3 +5,20 @@ function agregarpendiente () {
 	document.getElementById("lista").appendChild(nuevoElemento);
 	document.getElementById("agregarlista").value= ""
 }
+function agregarpendiente2 (){
+	var nuevopendiente2 = document.getElementById("crearlista").value;
+	var nuevoElemento2 = document.createElement("li");
+	nuevoElemento2.innerHTML = nuevopendiente2;
+	document.getElementById("lista2").appendChild(nuevoElemento2);
+	document.getElementById("crearlista").value=""
+}	
+// function borrarelemento(id){
+// 	pendiente = document.getElementById("id");
+// 	if (pendiente) {
+// 		alert("El elemento seleccionado se eliminara");
+// 	}else{
+// 		padre = pendiente.parentNode
+// 		padre.removeChild(pendiente);
+// 	}
+	
+// }
